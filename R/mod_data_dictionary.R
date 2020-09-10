02d645fa9c1e6413be3e4f3c2897a5a4a6a4b953
chriszheng2016 <chriszheng@vip.sina.com.cn>
2020-09-10 23:28:47 +0800

    ENH: add mod_data_dictionary
    ** source and tests

---
+++ R/mod_data_dictionary.R
@@ -0,0 +1,291 @@
+#' data_dictionary
+#'
+#' @description A shiny module for data_dictionary.
+#'
+#' @details
+#'  The module is an UI for user to ...
+#'
+#' @name data_dictionary
+#'
+#' @param id  An ID string of module to connecting UI function and Server
+#'   function.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # Set up control UI in app UI
+#' ui <- fluidPage(
+#'   data_dictionary_ui("data_dictionary_module")
+#' )
+#'
+#' # Call control server in App server
+#' server <- function(input, output, session) {
+#'   data_dictionary <- data_dictionary_server("data_dictionary_module")
+#' }
+#'
+#' # Run testing App for integration testing
+#' data_dictionary_app()
+#' }
+#'
+NULL
+
+#' UI function of data_dictionary
+#'
+#' @return * UI function doesn't return value.
+#'
+#' @describeIn data_dictionary  UI function of data_dictionary.
+#' @importFrom shiny NS tagList
+data_dictionary_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    fluidRow(
+      column(
+        1,
+        h4(strong("select codes:"))
+      ),
+      column(
+        10,
+        textInput(
+          inputId = ns("select_codes"),
+          label = NULL,
+          value = NULL,
+          width = "100%"
+        )
+      ),
+      column(
+        1,
+        actionButton(
+          inputId = ns("clear_codes"),
+          label = strong("Clear")
+        )
+      )
+    ),
+    tabsetPanel(
+      id = ns("dictionary_tabs"),
+      type = "tabs",
+      tabPanel(
+        "Stock Info",
+        DT::dataTableOutput(outputId = ns("stock_info_table"))
+      ),
+      tabPanel(
+        "Industry Info",
+        DT::dataTableOutput(outputId = ns("industry_info_table"))
+      ),
+      tabPanel(
+        "Factor Info",
+        DT::dataTableOutput(outputId = ns("factors_info_table"))
+      ),
+      tabPanel(
+        "Indicator Info",
+        DT::dataTableOutput(outputId = ns("indicators_info_table"))
+      )
+    )
+  )
+}
+
+#' Server function of data_dictionary
+#'
+#' @describeIn data_dictionary  Server function of data_dictionary.
+#' @return * Server function return a data frame of ...
+data_dictionary_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    # Data Source Reactive ----
+
+    stock_info <- reactive({
+      stock_db <- stock_db()
+      factors_info <- zstmodelr::get_stock_info(stock_db)
+    })
+
+    industry_info <- reactive({
+      stock_db <- stock_db()
+      factors_info <- zstmodelr::get_industry_info(stock_db)
+    })
+
+    factors_info <- reactive({
+      stock_db <- stock_db()
+      factors_info <- zstmodelr::get_factors_info(stock_db)
+    })
+
+    indicators_info <- reactive({
+      stock_db <- stock_db()
+      indicators_info <- zstmodelr::get_indicators_info(stock_db)
+    })
+
+    # Handle user interaction ----
+
+    # Handler for user to select code from table
+    user_select_codes <- function(DT_tableId, ds_info, id_var) {
+      select_index <- input[[glue::glue("{DT_tableId}_rows_selected")]]
+      select_codes <- dplyr::pull(ds_info[select_index, ], {{ id_var }})
+      if (length(select_codes) > 0) {
+        updateTextInput(
+          session = session,
+          inputId = "select_codes",
+          value = select_codes
+        )
+      }
+    }
+
+    observeEvent(input$stock_info_table_cell_clicked,
+      {
+        if (length(input$stock_info_table_cell_clicked) > 0) {
+          user_select_codes(
+            DT_tableId = "stock_info_table",
+            ds_info = stock_info(),
+            id_var = "stkcd"
+          )
+        }
+      },
+      ignoreNULL = TRUE,
+      ignoreInit = TRUE
+    )
+
+    observeEvent(input$industry_info_table_cell_clicked,
+                 {
+                   if (length(input$industry_info_table_cell_clicked) > 0) {
+                     user_select_codes(
+                       DT_tableId = "industry_info_table",
+                       ds_info = industry_info(),
+                       id_var = "indcd"
+                     )
+                   }
+                 },
+                 ignoreNULL = TRUE,
+                 ignoreInit = TRUE
+    )
+
+    observeEvent(input$factors_info_table_cell_clicked,
+      {
+        if (length(input$factors_info_table_cell_clicked) > 0) {
+          user_select_codes(
+            DT_tableId = "factors_info_table",
+            ds_info = factors_info(),
+            id_var = "factor_code"
+          )
+        }
+      },
+      ignoreNULL = TRUE,
+      ignoreInit = TRUE
+    )
+
+    observeEvent(input$indicators_info_table_cell_clicked,
+      {
+        if (length(input$indicators_info_table_cell_clicked) > 0) {
+          user_select_codes(
+            DT_tableId = "indicators_info_table",
+            ds_info = indicators_info(),
+            id_var = "ind_code"
+          )
+        }
+      },
+      ignoreNULL = TRUE,
+      ignoreInit = TRUE
+    )
+
+    observeEvent(input$clear_codes, {
+
+      # Select codes input
+      updateTextInput(
+        session = session,
+        inputId = "select_codes",
+        value = ""
+      )
+
+      dt_tables <- c(
+        "stock_info_table", "industry_info_table",
+        "factors_info_table", "indicators_info_table"
+      )
+
+      # Clear selections in tables
+      for (dt_table in dt_tables) {
+        proxy <- DT::dataTableProxy(dt_table)
+        DT::selectRows(proxy, selected = NULL)
+      }
+    })
+
+
+    # Output UI -----
+    output$stock_info_table <- DT::renderDataTable({
+      DT::datatable(
+        stock_info(),
+        filter = "top",
+        extensions = "Scroller",
+        options = list(
+          columnDefs = list(list(className = "dt-center")),
+          pageLength = 20,
+          dom = "ltir",
+          deferRender = TRUE,
+          scrollY = 650,
+          scrollX = TRUE,
+          scroller = TRUE
+        )
+      )
+    })
+
+    output$industry_info_table <- DT::renderDataTable({
+      DT::datatable(
+        industry_info(),
+        filter = "top",
+        extensions = "Scroller",
+        options = list(
+          columnDefs = list(list(className = "dt-center")),
+          pageLength = 20,
+          dom = "ltir",
+          deferRender = TRUE,
+          scrollY = 650,
+          scrollX = TRUE,
+          scroller = TRUE
+        )
+      )
+    })
+
+    output$factors_info_table <- DT::renderDataTable({
+      DT::datatable(
+        factors_info(),
+        filter = "top",
+        extensions = "Scroller",
+        options = list(
+          columnDefs = list(list(className = "dt-center")),
+          pageLength = 20,
+          dom = "ltir",
+          deferRender = TRUE,
+          scrollY = 650,
+          scrollX = TRUE,
+          scroller = TRUE
+        )
+      )
+    })
+
+    output$indicators_info_table <- DT::renderDataTable({
+      DT::datatable(
+        indicators_info(),
+        filter = "top",
+        extensions = "Scroller",
+        options = list(
+          columnDefs = list(list(className = "dt-center")),
+          pageLength = 20,
+          dom = "ltir",
+          deferRender = TRUE,
+          scrollY = 650,
+          scrollX = TRUE,
+          scroller = TRUE
+        )
+      )
+    })
+  })
+}
+
+#' Testing module app of data_dictionary
+#'
+#' @describeIn data_dictionary  Testing App of data_dictionary.
+data_dictionary_app <- function() {
+  ui <- fluidPage(
+    data_dictionary_ui("data_dictionary_module")
+  )
+  server <- function(input, output, session) {
+    data_dictionary <- data_dictionary_server("data_dictionary_module")
+  }
+  shinyApp(ui, server)
+}
---
+++ tests/testthat/test-mod_data_dictionary.R
@@ -0,0 +1,73 @@
+# Tests for module of data_dictionary  ----
+options(testthat.edition_ignore = TRUE)
+
+context("Tests for module of data_dictionary")
+
+
+# Set up test environment
+
+test_that("data_dictionary_server - reactives and output updates", {
+  testServer(data_dictionary_server,
+    #args = list(factors_info = reactive(factors_info)),
+    {
+      # data_dictionary_server with typical user inputs ====
+
+      # -- Sample Code for reference --
+      # Set input for data_dictionary
+      # select_factors <- c("CR", "QR")
+      # session$setInputs(
+      #   factor_groups = "Financial Risk",
+      #   select_factors = select_factors,
+      #   data_dictionary = 1 # Non-null value indicating use click button
+      # )
+      # Check data_dictionary()
+      # data_dictionary <- data_dictionary()
+      # expect_s3_class(data_dictionary, "data.frame")
+      # expect_fields <- c(c("date", "period", "stkcd", "indcd"), select_factors)
+      # actual_fields <- names(data_dictionary
+      # expect_true(all(actual_fields %in% expect_fields))
+      # expect_true(nrow(data_dictionary >= 0)
+      # Check output
+      # expect_snapshot_output(output$factors_info_table)
+      # expect_snapshot_output(output$factors_data_table)
+
+    }
+  )
+})
+
+test_that("data_dictionary_app - Module App works", {
+  skip_on_cran()
+  skip_on_ci()
+  skip_on_covr()
+
+  test_app_file <- "app.R"
+  withr::with_file(test_app_file, {
+
+    # Set up temp app.R for loading App
+    writeLines("pkgload::load_all()\ndata_dictionary_app()",
+      con = test_app_file
+    )
+
+    # Load test App
+    suppressWarnings({
+      app <- shinytest::ShinyDriver$new(".", loadTimeout = 1000 * 100)
+    })
+
+
+    # data_dictionary_app with typical user inputs ====
+
+    # -- Sample Code for reference --
+    # select_factors <- c("CR", "QR")
+    # app$setInputs(
+    #   `data_dictionary_module-factor_groups` = "Financial Risk",
+    #   `data_dictionary_module-factors_in_group` = select_factors,
+    #   `data_dictionary_module-data_dictionary` = "click",
+    #   timeout_ = 1000 * 10
+    # )
+    # expect_snapshot_value(app$getAllValues(), style = "json2")
+    # expect_snapshot_value(app$getAllValues(), style = "serialize")
+
+    app$stop()
+  })
+})
+
