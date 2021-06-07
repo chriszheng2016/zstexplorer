fac6784bb51c0b5c95b09843aa83463dc12c6a81
chriszheng2016 <chriszheng@vip.sina.com.cn>
2021-06-07 10:26:34 +0800

    TST: refactor skip_if_stock_db_not_ready
    Move orginal code of skip_if_stock_db_not_ready to zstmodelr package. Change it as a wrapper of zstmodelr::skip_if_stock_db_not_ready()
    
    [ticket: X]

--- R/utils_testthat_helpers.R
+++ R/utils_testthat_helpers.R
@@ -1,26 +1,9 @@
+# Utility functions of helpers for testthat ----
 
 # Skip tests if stock db is not ready
+
+# This wrapper of [zstmodelr::skip_if_stock_db_not_ready()] to skip tests
+# if stock db is not available for testing.
 skip_if_stock_db_not_ready <- function(dsn = get_golem_config("database_dsn")) {
-
-  # NO_STOCK_DB is used to simulate stock db is not unavailable
-  no_stock_db <- identical(Sys.getenv("NO_STOCK_DB"), "true")
-
-  if(!no_stock_db){
-    # Test stock database is ready when stock db is avaliable
-    stock_db <- zstmodelr::stock_db(zstmodelr::gta_db, dsn)
-    suppressMessages(db_ready <- zstmodelr::open_stock_db(stock_db))
-    withr::defer({
-      suppressMessages(zstmodelr::close_stock_db(stock_db))
-    })
-  } else {
-    db_ready <- FALSE
-  }
-
-  # Skip if stock database is not ready
-  testthat::skip_if_not(db_ready,
-    message = sprintf(
-      "DSN(%s) is not ready, skip tests related to stock database ",
-      dsn
-    )
-  )
+  zstmodelr::skip_if_stock_db_not_ready(dsn)
 }
--- tests/testthat/test-utils_testthat_helpers.R
+++ tests/testthat/test-utils_testthat_helpers.R
@@ -2,19 +2,21 @@
 
 # context("Tests for helper functions for testthat")
 
-skip_if_stock_db_not_ready()
 
 test_that("skip_if_stock_db_not_ready, with various arguments", {
 
+  # Skip following tests in environment without stock db
+  skip_if_stock_db_not_ready()
+
   # skip_if_stock_db_not_ready with default arguments ====
 
-  # A skip happens when stock db is able to be connected
+  # A skip happens when stock db is unable to be connected
   mockery::stub(skip_if_stock_db_not_ready,
                 what = "get_golem_config",
                 how = "GTA_SQLData1")
   expect_condition(skip_if_stock_db_not_ready(), class = "skip")
 
-  # A skip doesn't happen when stock db is unable to be connected
+  # A skip doesn't happen when stock db is able to be connected
   mockery::stub(skip_if_stock_db_not_ready,
                 what = "get_golem_config",
                 how = "GTA_SQLData")
